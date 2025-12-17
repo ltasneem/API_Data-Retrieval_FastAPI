7fa6e861a0ff7b871d2ecda57f761530a30203f0
Lubaba Nuzhat Tasneem <lubaba070@gmail.com>
2025-12-16 22:55:15 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -14,10 +14,28 @@ from sqlalchemy.orm import clear_mappers
 
 from models.request.*
 from models.response.*
+from core_common.*
 from queries.ResultSetToJsonConversion import *
+from query_builder import *
+from payloadToRequestConversion import *
+
 
 #instantiate memory profiler
 profiler_logstream = memory_profiler.LogFile("memory_logs", True)
 
 #call fastapi
 app = fastapi.FastAPI(title="Azure Function to test DB data", debug=True, docs_url=None)
+
+@app.post("/pprequests", name = "Get PersonProfile Data", response_model=Page[PersonProfileResponse])
+async def personprofilerequests(code:str, payload: Any = Body(None)):
+  payloadconverted = json.dumps(payload)
+  request = personProfileToRequestConversion(payloadconverted)
+
+  conn = engine.connect()
+  map_personnel_personprofile(request,conn)
+
+  personprofileResult = list[PersonProfileResponse] = personProfileQuery(request, conn)
+
+  return paginate(PersonProfileResponseResult)
+
+add_pagination(app)
