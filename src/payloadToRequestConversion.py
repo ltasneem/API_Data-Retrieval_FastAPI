d3b95b8267fa55bfea85295ea6d7d3b29258227f
Lubaba Nuzhat Tasneem <lubaba070@gmail.com>
2025-12-16 22:07:18 -0500

    Update payloadToRequestConversion.py

--- src/payloadToRequestConversion.py
+++ src/payloadToRequestConversion.py
@@ -1,1 +1,11 @@
+from sqlalchemy.orm import clear_mappers
+import json
 
+
+def personProfileRequestConversion(payloadconverted: str):
+  json_object = json.loads(payloadconverted)
+  #parsing the request
+  clear_mappers()
+  request =PersonProfileRequest
+  
+  return request
