7a7bd42648afd6b001cfddea18e59a85009a6e41
Lubaba Nuzhat Tasneem <lubaba070@gmail.com>
2025-12-16 22:16:54 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -12,3 +12,12 @@ from fastapi_pagination import paginate, Page, add_pagination
 from datetime import datetime, timedelta
 from sqlalchemy.orm import clear_mappers
 
+from models.request.*
+from models.response.*
+from queries.ResultSetToJsonConversion import *
+
+#instantiate memory profiler
+profiler_logstream = memory_profiler.LogFile("memory_logs", True)
+
+#call fastapi
+app = fastapi.FastAPI(title="Azure Function to test DB data", debug=True, docs_url=None)
